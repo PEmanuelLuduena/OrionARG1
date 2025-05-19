3743b5417811ed8179530a2e488f618873902d5e
Pablo <pluduena544@gmail.com>
2025-05-19 15:51:05 -0300

    Descripción clara de los cambios

--- script.js
+++ script.js
@@ -12,3 +12,29 @@ document.getElementById('contact-form').addEventListener('submit', function(even
       alert('Error al enviar el correo: ' + JSON.stringify(error));
     });
 });
+
+// Función para obtener parámetros de la URL
+function getParam(param) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(param);
+}
+
+// Al cargar la página
+window.onload = function() {
+  const user_name = getParam('user_name');
+  const user_email = getParam('user_email');
+  const message = getParam('message');
+
+  if(user_name && user_email && message) {
+    emailjs.send("tu_service_id", "tu_template_id", {
+      user_name: user_name,
+      user_email: user_email,
+      message: message
+    })
+    .then(() => {
+      alert("Correo enviado con éxito!");
+    }, (error) => {
+      alert("Error al enviar el correo: " + JSON.stringify(error));
+    });
+  }
+};
