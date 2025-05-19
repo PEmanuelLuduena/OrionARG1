f5d95356293b3b810865c70c6d6883adae0b963e
Pablo <pluduena544@gmail.com>
2025-05-19 16:18:23 -0300

    Formulario de contacto funcionando

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-document.getElementById('formulario').addEventListener('submit', function(event) {
-  event.preventDefault(); // Evita que la página se recargue
-  emailjs.sendForm('service_acw0v47', 'template_rlfq6yj', this)
-    .then(function() {
-      alert('¡Email enviado con éxito!');
-    }, function(error) {
-      alert('Error al enviar el email: ' + JSON.stringify(error));
-    });
-});
+// script.js
+
+window.onload = function () {
+  document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    // Datos de EmailJS
+    const serviceID = 'service_acw0v47';  // <-- Cambiar por tu Service ID
+    const templateID = 'template_yaewf3f';  // <-- Cambiar por tu Template ID
+    const publicKey = 'S-XdyH2yWbfy46oVk';  // <-- Cambiar por tu Public Key
+
+    // Enviar email
+    emailjs.sendForm(serviceID, templateID, this, publicKey)
+      .then(() => {
+        alert('¡Correo enviado correctamente!');
+      },
